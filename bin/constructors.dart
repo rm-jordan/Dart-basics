70a9dba2236a3582c21a4999d857c70b4cc23094
rm-jordan <64985248+rm-jordan@users.noreply.github.com>
2022-06-05 10:11:03 -0400

    added constructor and interface templates

---
+++ bin/constructors.dart
@@ -0,0 +1,35 @@
+class Rectangle {
+  final int width;
+  final int height;
+  String? name;
+  late final int area;
+
+  Rectangle(this.width, this.height, [this.name]) {
+    area = width * height;
+  }
+}
+
+class Circle {
+  const Circle({required int radius, String? name});
+}
+
+const cir = Circle(radius: 50, name: 'foo');
+
+class Point {
+  double lat = 0;
+  double lng = 0;
+
+  // Named constructor
+  Point.fromMap(Map data) {
+    lat = data['lat'];
+    lng = data['lng'];
+  }
+
+  Point.fromList(List data) {
+    lat = data[0];
+    lng = data[1];
+  }
+}
+
+var p1 = Point.fromMap({'lat': 23, 'lng': 50});
+var p2 = Point.fromList([23, 50]);
---
+++ bin/interfaces.dart
@@ -0,0 +1,16 @@
+class Elephant {
+  // Public interface
+  final String name;
+
+  // In the interface, but visible only in this library. (private)
+  final int _id = 23;
+
+  // Not in the interface, since this is a constructor.
+  Elephant(this.name);
+
+  // Public method.
+  sayHi() => 'My name is $name.';
+
+  // Private method.
+  _saySecret() => 'My ID is $_id.';
+}
